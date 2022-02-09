33ffb31d645f7f6d1c13d009d6d463d662d6a1fe
Jeff Mahoney <jeffm@suse.com>
2022-02-08 19:56:24 -0500

    move plugin to linux

--- vql/parsers/sdjournal/no_systemd.go
+++
@@ -1,4 +0,0 @@
-// The sdjournal package exists only to target the systemd journal, which
-// does not exist on Windows or MacOS.  This file exists to avoid the following
-// build failure: "build constraints exclude all Go files"
-package sdjournal
