28739650e5c31866855e24c71b3e1e5a47344cd6
Jeff Mahoney <jeffm@suse.com>
2023-02-05 05:57:27 -0500

    vql/functions/hash: cache results on Linux
    When we use Velociraptor on systems with a big fork/exec workload,
    use system call auditing, and hash the executable, we end up with
    pinned CPUs for obvious reasons.  There's no value in hashing the same
    file over and over if we know it hasn't changed.
    
    This commit adds an LRU cache for the hash results.  It uses the
    dev number, inode number, mtime, and ctime to ensure that the file
    hasn't been modified.  A race does exist betewen stat and open, but
    for this use case a bigger race exists between exec and the hash
    being requested.

--- vql/functions/hash.go
+++ vql/functions/hash.go
@@ -32,6 +32,7 @@ import (
 	vql_subsystem "www.velocidex.com/golang/velociraptor/vql"
 	"www.velocidex.com/golang/vfilter"
 	"www.velocidex.com/golang/vfilter/arg_parser"
+	"www.velocidex.com/golang/velociraptor/third_party/cache"
 )
 
 var (
@@ -52,11 +53,63 @@ type HashResult struct {
 	sha256 hash.Hash
 }
 
-func (self *HashResult) ToDict() *ordereddict.Dict {
-	return ordereddict.NewDict().
-		Set("MD5", self.MD5).
-		Set("SHA1", self.SHA1).
-		Set("SHA256", self.SHA256)
+type HashResultCacheEntry interface {
+	cache.Value
+	Validate(filename string) (bool, error)
+	Result() *HashResult
+}
+
+type HashResultCache struct {
+	scope		vfilter.Scope
+	lru_cache	*cache.LRUCache
+}
+
+func (self *HashResultCache) Get(filename string) (HashResultCacheEntry, bool) {
+	if cacheSize > 0 {
+		res, ok := self.lru_cache.Get(filename)
+		if ok {
+			return res.(HashResultCacheEntry), ok
+		}
+	}
+
+	return nil, false
+}
+
+func (self *HashResultCache) Set(filename string, entry HashResultCacheEntry) {
+	if cacheSize > 0 {
+		self.lru_cache.Set(filename, entry)
+	}
+}
+
+func (self *HashResultCache) Delete(filename string) {
+	if cacheSize > 0 {
+		_ = self.lru_cache.Delete(filename)
+	}
+}
+
+var mu sync.Mutex
+
+func GetHashResultCache(scope vfilter.Scope) *HashResultCache {
+	if cacheSize <= 0 {
+		return nil
+	}
+
+	defer mu.Unlock()
+	mu.Lock()
+	key := "hash_result_cache"
+	root_scope := vql_subsystem.GetRootScope(scope)
+
+	cache_ctx, ok := vql_subsystem.CacheGet(root_scope, key).(*HashResultCache)
+	if !ok {
+		cache_ctx = &HashResultCache{
+			lru_cache: cache.NewLRUCache(cacheSize),
+			scope: scope,
+		}
+		root_scope.AddDestructor(func() { cache_ctx.lru_cache.Clear() })
+		vql_subsystem.CacheSet(root_scope, key, cache_ctx)
+	}
+
+	return cache_ctx
 }
 
 type HashFunctionArgs struct {
@@ -79,6 +132,7 @@ func (self *HashFunction) Call(ctx context.Context,
 		return vfilter.Null{}
 	}
 
+
 	cached_buffer := pool.Get().(*[]byte)
 	defer pool.Put(cached_buffer)
 
@@ -96,30 +150,54 @@ func (self *HashFunction) Call(ctx context.Context,
 		return vfilter.Null{}
 	}
 
-	file, err := fs.Open(arg.Path.String())
-	if err != nil {
-		//scope.Log("hash %s: %v", arg.Path.String(), err)
-		return vfilter.Null{}
-	}
-	defer file.Close()
+	filename := arg.Path.String()
 
-	result := &HashResult{}
+	cache := GetHashResultCache(scope)
+
+	var result *HashResult
+	var entry HashResultCacheEntry
+	if cache != nil {
+		entry, ok := cache.Get(filename)
+		if ok {
+			ok, err = entry.Validate(filename)
+			if !ok {
+				cache.Delete(filename)
+			}
+			if err != nil {
+				scope.Log("hash: %v: %v", filename, err)
+				return vfilter.Null{}
+			}
+		}
+
+		if !ok {
+			entry, err = newHashResultCacheEntry(filename)
+			if err != nil {
+				scope.Log("hash: %v: %v", filename, err)
+				return vfilter.Null{}
+			}
+		}
+
+		result = entry.Result()
+	} else {
+		result = &HashResult{}
+	}
+
+	var want_md5, want_sha1, want_sha256 bool
+	var need_md5, need_sha1, need_sha256 bool
 
 	if arg.HashSelect == nil {
-		result = &HashResult{
-			md5:    md5.New(),
-			sha1:   sha1.New(),
-			sha256: sha256.New(),
-		}
+		want_md5 = true
+		want_sha1 = true
+		want_sha256 = true
 	} else {
 		for _, hash_opt := range arg.HashSelect {
 			switch hash_opt {
 			case "sha256", "SHA256":
-				result.sha256 = sha256.New()
+				want_sha256 = true
 			case "sha1", "SHA1":
-				result.sha1 = sha1.New()
+				want_sha1 = true
 			case "md5", "MD5":
-				result.md5 = md5.New()
+				want_md5 = true
 			default:
 				scope.Log("hashselect option %s not recognized (should be md5, sha1, sha256)",
 					hash_opt)
@@ -128,55 +206,97 @@ func (self *HashFunction) Call(ctx context.Context,
 		}
 	}
 
-	for {
-		select {
-		case <-ctx.Done():
+	if want_md5 && result.md5 == nil {
+		need_md5 = true
+		result.md5 = md5.New()
+	}
+
+	if want_sha1 && result.sha1 == nil {
+		need_sha1 = true
+		result.sha1 = sha1.New()
+	}
+
+	if want_sha256 && result.sha256 == nil {
+		need_sha256 = true
+		result.sha256 = sha256.New()
+	}
+
+	if need_md5 || need_sha1 || need_sha256 {
+		file, err := fs.Open(filename)
+		if err != nil {
+			scope.Log("hash: %s: %v", filename, err)
 			return vfilter.Null{}
+		}
+		defer file.Close()
 
-		default:
-			n, err := file.Read(buf)
+		done := false
+		for !done {
+			select {
+			case <-ctx.Done():
+				return vfilter.Null{}
 
-			// We are done!
-			if n == 0 || err == io.EOF {
-				if n == 0 {
-					if result.md5 != nil {
-						result.MD5 = fmt.Sprintf(
-							"%x", result.md5.Sum(nil))
+			default:
+				n, err := file.Read(buf)
+
+				// We are done!
+				if n == 0 || err == io.EOF {
+					if n == 0 {
+						done = true
+						break
 					}
-
-					if result.sha1 != nil {
-						result.SHA1 = fmt.Sprintf(
-							"%x", result.sha1.Sum(nil))
-					}
-
-					if result.sha256 != nil {
-						result.SHA256 = fmt.Sprintf(
-							"%x", result.sha256.Sum(nil))
-					}
-					return result.ToDict()
+				} else if err != nil {
+					scope.Log("hash: %v", err)
+					return vfilter.Null{}
 				}
 
-			} else if err != nil {
-				scope.Log("hash: %v", err)
-				return vfilter.Null{}
-			}
+				if need_md5 {
+					_, _ = result.md5.Write(buf[:n])
+				}
 
-			if result.md5 != nil {
-				_, _ = result.md5.Write(buf[:n])
-			}
+				if need_sha1 {
+					_, _ = result.sha1.Write(buf[:n])
+				}
 
-			if result.sha1 != nil {
-				_, _ = result.sha1.Write(buf[:n])
-			}
+				if need_sha256 {
+					_, _ = result.sha256.Write(buf[:n])
+				}
 
-			if result.sha256 != nil {
-				_, _ = result.sha256.Write(buf[:n])
+				// Charge an op for each buffer we read
+				scope.ChargeOp()
 			}
+		}
 
-			// Charge an op for each buffer we read
-			scope.ChargeOp()
+		if need_md5 {
+			result.MD5 = fmt.Sprintf("%x", result.md5.Sum(nil))
+		}
+
+		if need_sha1 {
+			result.SHA1 = fmt.Sprintf("%x", result.sha1.Sum(nil))
+		}
+
+		if need_sha256 {
+			result.SHA256 = fmt.Sprintf("%x", result.sha256.Sum(nil))
+		}
+
+		if cache != nil {
+			cache.Set(filename, entry)
 		}
 	}
+
+	row := ordereddict.NewDict()
+	if want_md5 {
+		row.Set("MD5", result.MD5)
+	}
+
+	if want_sha1 {
+		row.Set("SHA1", result.SHA1)
+	}
+
+	if want_sha256 {
+		row.Set("SHA256", result.SHA256)
+	}
+
+	return row
 }
 
 func (self HashFunction) Info(scope vfilter.Scope, type_map *vfilter.TypeMap) *vfilter.FunctionInfo {
